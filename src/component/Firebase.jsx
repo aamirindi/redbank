c27fd68923ffcf989c32dae72e850046eda47664
Mohd Aamir Indi <90147104+aamirindi@users.noreply.github.com>
2023-09-30 22:12:17 +0530

    Update Firebase.jsx

--- src/component/Firebase.jsx
+++ src/component/Firebase.jsx
@@ -3,7 +3,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBU7_jD-ciC5kb1K9w4864NmcCWKYtGc5w",
+  apiKey: "",
   authDomain: "red-bank-81590.firebaseapp.com",
   projectId: "red-bank-81590",
   storageBucket: "red-bank-81590.appspot.com",
